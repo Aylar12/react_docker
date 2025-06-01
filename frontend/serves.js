94614a39fd290378358dddeab8d3d58bb00a130c
Aylar12 <ailar.jafarian@gmail.com>
2025-06-01 19:48:33 +0330

    add node exporter

---
+++ frontend/serves.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
