5e59f538f94491a1b0c96619f441757d5f02cb29
Aylar12 <ailar.jafarian@gmail.com>
2025-06-01 19:28:21 +0330

    add node exporter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,6 @@ const App = () => {
   return <div><h1>Hello Docker!</h1></div>;
 
 };
-// server.js
-const express = require('express');
-const client = require('prom-client');
-
-const app = express();
-const register = new client.Registry();
-
-client.collectDefaultMetrics({ register });
-
-app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(await register.metrics());
-});
-
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
 
 
 
